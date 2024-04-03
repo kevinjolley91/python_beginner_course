d9ab98e5acbd1fea25c3e0068bac6ad6f10d9f78
kevinjolley91 <jolley427@gmail.com>
2024-04-03 15:08:21 -0400

    learning about using variables in strings

---
+++ using_variables_in_strings.py
@@ -0,0 +1,11 @@
+day = 3
+month = 'April'
+temp = 45
+
+# print(f"Today is {month} {day}, and it's {temp} degrees outside.")
+
+# Make a variable called day_name and set it to day of the week like Tuesday and add it to the string we print
+
+day_name = "Wednesday"
+
+print(f"Today is {day_name}, {month} {day}, and it's {temp} degrees outside.")
