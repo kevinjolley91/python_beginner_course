295a0bb5540af78833b2350164c8813faa8cb865
kevinjolley91 <jolley427@gmail.com>
2024-04-03 13:40:58 -0400

    learning about integers and floating point values

---
+++ ints_and_floats.py
@@ -0,0 +1,14 @@
+day = 21
+
+temp = -15
+
+weight = 190.4
+
+print(weight * 2)
+
+# Find something around you in your life to represent an int and a float. Put them in variables.
+
+battery = 83
+
+bottle = 0.1
+
