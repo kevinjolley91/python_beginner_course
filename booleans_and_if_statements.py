73f72361039c25bc94a06254d64561da94dfd30d
kevinjolley91 <jolley427@gmail.com>
2024-04-03 15:18:21 -0400

    learning about booleans and if statements

---
+++ booleans_and_if_statements.py
@@ -0,0 +1,15 @@
+day = 21
+weight = 190.4
+month = 'October'
+
+light_is_on = True
+
+if light_is_on:
+    print("The light is on!")
+    
+# Find something around you that could be represented by a Boolean and make a variable for it
+
+# bottle_is_full = False
+
+# if bottle_is_full:
+#     print("Take a drink!")
