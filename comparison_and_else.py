a764fba3a5c6a6b3605de50c23b285134f650603
kevinjolley91 <jolley427@gmail.com>
2024-04-03 15:30:05 -0400

    learning about comparisons and else statements

---
+++ comparison_and_else.py
@@ -0,0 +1,20 @@
+light_is_on = True
+
+if light_is_on:
+    print("The light is on!")
+else:
+    print("We're in the dark.")
+    
+weight = 190.4
+
+if weight <= 195:
+    print("You're under weight :)")
+    
+# Make a variable age, and if someone is 18 years or older, print that they are an adult. Otherwise print that they are a child.
+
+age = 33
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a child.")
