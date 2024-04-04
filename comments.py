39f4c632222eed9bb0e0870473acb6f4ff8d1ff8
kevinjolley91 <jolley427@gmail.com>
2024-04-04 16:53:46 -0400

    learning about comments

---
+++ comments.py
@@ -0,0 +1,12 @@
+wallet = 40
+
+wallet -= 8 # I bought some food
+
+wallet += 40 # I got a payday!
+
+# I need to figure out how to uppercase this
+'nick'.upper()
+
+# Create a comment! Like this one :)
+
+# I'm enjoying this class! It's working as a good refresher to Python!
