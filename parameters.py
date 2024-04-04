2d30fc065cd3d6ecdb811454410be631f282d0a9
kevinjolley91 <jolley427@gmail.com>
2024-04-04 16:34:04 -0400

    learning about adding parameters to functions

---
+++ parameters.py
@@ -0,0 +1,17 @@
+def hello(name):
+    print(f"Hello {name}!")
+    
+# hello("Kevin")
+
+def add_numbers(num1, num2):
+    print(num1 + num2)
+    
+# add_numbers(4, 8)
+# add_numbers(3, 7)
+
+# Create a function called dog_info that takes in a dog's age and name and prints a sentence about the dog
+
+def dog_info(name, age):
+    print(f"The dog named {name} is {age} years old.")
+    
+dog_info("Fido", 7)
