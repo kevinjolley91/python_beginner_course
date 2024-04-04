a897b3abbdf90232615eb7ef3a575dd2c53e7a45
kevinjolley91 <jolley427@gmail.com>
2024-04-04 15:33:27 -0400

    learning about creating functions

---
+++ functions_2.py
@@ -0,0 +1,13 @@
+def bark():
+    print("Woof woof!")
+    print("I'm a dog!")
+    
+for x in range(100):
+    bark()
+    
+# Create a function called hello that prints "Hello Nick!"
+
+def hello():
+    print("Hello Nick!")
+    
+hello()
