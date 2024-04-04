88fefb720f892f454f64cd7dd8a5a21fc6731f67
kevinjolley91 <jolley427@gmail.com>
2024-04-04 14:35:16 -0400

    learning about for loops

---
+++ for_loops.py
@@ -0,0 +1,12 @@
+for number in range(100000):
+    print(number)
+    
+fav_movies = ["Sandlot", "The Lego Movie", "Dune"]
+
+for movie in fav_movies:
+    print(movie)
+    
+# Loop 40 times and print the number of the loop times 2. Ex. 2,4,6,8...
+
+for x in range(40):
+    print(x * 2)
