350a8c6337ad3f2cb1389740367828c0756585e5
kevinjolley91 <jolley427@gmail.com>
2024-04-04 14:42:52 -0400

    learning about dictionaries which are like hashes in ruby

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+cats = {"Jane":6, "Tom":14, "Sara":8}
+
+cats["Wilson"] = 1
+
+del(cats["Tom"])
+
+print(len(cats))
+
+# Create a dictionary with Ints for keys and Booleans for values
+
+even_numbers = {1:False, 2:True, 3:False, 4:True, 5:False}
