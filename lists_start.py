735e0b6f9bc7a47c4e3cdc41bfd4546abc17c5f8
kevinjolley91 <jolley427@gmail.com>
2024-04-04 11:57:48 -0400

    learning about lists which are very much like arrays

---
+++ lists_start.py
@@ -0,0 +1,9 @@
+fav_movies = ["Sandlot", "The Lego Movie", "Dune"]
+
+print(fav_movies[0])
+
+# Make a list of your 3 favorite numbers and print the first number from your list
+
+fav_numbers = [6.2, 650, 1320]
+
+print(fav_numbers[0])
